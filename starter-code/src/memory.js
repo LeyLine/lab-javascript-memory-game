0b7cb31fec88cb36311af17d79f83b91504e3a26
Adrian Llerena <a.llerena001@gmail.com>
2018-04-22 10:20:26 -0400

    last proto almost done'

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,34 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function(cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function(cardsArr) {
+  this.cards = cardsArr;
+  var m = cardsArr.length,
+    t,
+    i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = cardsArr[m];
+    cardsArr[m] = cardsArr[i];
+    cardsArr[i] = t;
+  }
+  return this.cards;
+};
 
-// };
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard) {
+  this.pairsClicked += 1;
+  if (firstCard === secondCard) {
+    this.pairsGuessed += 1;
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
-
-// }
-
-// MemoryGame.prototype.finished = function () {
-
-// };
+MemoryGame.prototype.finished = function() {
+  return false;
+};
